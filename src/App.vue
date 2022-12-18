1a7a454c3a236722c6ef106932805068b5eab3ff
rafael <rafaelcnunes@icloud.com>
2022-12-18 00:36:36 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+    <TheNavbar />
     <router-view />
   </div>
 </template>
+
+<script>
+const TheNavbar = () => import("@/components/TheNavbar.vue");
+
+export default {
+  name: "App",
+  components: {
+    TheNavbar,
+  },
+};
+</script>
 
 <style lang="scss">
 #app {
@@ -15,18 +23,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "TheNavbar",
+};
+</script>
+
+<style lang="scss" scoped>
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
