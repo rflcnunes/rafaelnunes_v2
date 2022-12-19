a439ab46b7b69ba447812efb54bf6d4eeff5c981
rafael <rafaelcnunes@icloud.com>
2022-12-19 01:30:22 -0300

    added navbar and scss variables

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,9 +1,12 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/works">Works</router-link> |
-    <router-link to="/skills">Skills</router-link> |
-    <router-link to="/about">About</router-link>
+    <img id="icon" src="@/assets/robot_icon.svg" alt="robot_icon" />
+    <div id="options">
+      <router-link to="/">Home</router-link>
+      <router-link to="/works">Works</router-link>
+      <router-link to="/skills">Skills</router-link>
+      <router-link to="/about">About</router-link>
+    </div>
   </nav>
 </template>
 
@@ -14,15 +17,37 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "@/style/scss/_variables.scss";
+
 nav {
   padding: 30px;
+  font-family: $roboto-family;
+  display: flex;
+  justify-content: space-between;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+  #icon {
+    width: 50px;
+    height: 50px;
+    margin-right: 20px;
+  }
 
-    &.router-link-exact-active {
-      color: #42b983;
+  #options {
+    width: 50%;
+    padding: 4px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    a {
+      font-weight: $font-weight-medium;
+      color: $color-primary-dark;
+      text-decoration: none;
+
+      &.router-link-exact-active {
+        color: $color-green;
+        font-weight: $font-weight-xbold;
+        text-decoration: dashed underline;
+      }
     }
   }
 }
