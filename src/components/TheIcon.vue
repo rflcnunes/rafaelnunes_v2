a1692008c730d500a883f8231acf154dc1a2b45f
rafael <rafaelcnunes@icloud.com>
2022-12-19 04:01:25 -0300

    added TheIcon component and Material Icons cdn

---
+++ src/components/TheIcon.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <span class="material-icons">{{ icon }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TheIcon",
+  props: {
+    icon: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+//
+</style>
