586884f2ad9c4c0bb5e6925ebe9bb485621be59f
rafael <rafaelcnunes@icloud.com>
2022-12-20 02:08:09 -0300

    added dark mode in the HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="home">
-    <!--  -->
+  <div class="home" :class="theme">
+    {{ theme }}
   </div>
 </template>
 
@@ -9,8 +9,36 @@
 
 export default {
   name: "HomeView",
+  data() {
+    return {
+      darkMode: false,
+    };
+  },
   components: {
     //
   },
+  created() {
+    this.darkMode = localStorage.getItem("theme") === "dark_mode";
+  },
+  updated() {
+    this.darkMode = localStorage.getItem("theme") === "dark_mode";
+  },
+  computed: {
+    theme() {
+      return this.darkMode ? "home-dark" : "home-light";
+    },
+  },
 };
 </script>
+
+<style scoped>
+.home-dark {
+  background-color: #1a1a1a;
+  color: #fff;
+}
+
+.home-light {
+  background-color: #fff;
+  color: #000;
+}
+</style>
